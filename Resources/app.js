73dc78df5234c24f7ebe2ddd6019f22d7509361b
Björn Schotte <bjoern.schotte@googlemail.com>
2012-02-18 20:40:31 +0100

    changed window 2 explanation

--- Resources/app.js
+++ Resources/app.js
@@ -43,7 +43,7 @@ var tab2 = Titanium.UI.createTab({
 
 var label2 = Titanium.UI.createLabel({
 	color:'#999',
-	text:'I am Window 2',
+	text:'Da schau her, ich bin Fenster 2',
 	font:{fontSize:20,fontFamily:'Helvetica Neue'},
 	textAlign:'center',
 	width:'auto'
